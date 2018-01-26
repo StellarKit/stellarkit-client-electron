a16dd841e2bde07a07be05c5af3a0d07dbb6dfbd
Steve Gehrman <sgehrman@gmail.com>
2018-01-25 22:14:18 -0800

    sfd

---
+++ src/components/XLMRocket.vue
@@ -0,0 +1,127 @@
+<template>
+<div class='rocket-main'>
+  <div class='rocket-sidebar'>
+    <v-menu open-on-hover bottom offset-y>
+      <v-btn @click.native='buttonClick("menu")' icon dark slot="activator">
+        <v-icon>menu</v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title" @click="buttonClick(item.cmd)">
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+    <v-btn @click.native='buttonClick("send")' icon dark>
+      <v-icon>monetization_on</v-icon>
+    </v-btn>
+    <v-btn @click.native='buttonClick("balances")' icon dark>
+      <v-icon>account_balance</v-icon>
+    </v-btn>
+  </div>
+
+  <div class='rocket-content'>
+    <div v-if="dialogMode === 'main'">
+      <div class='rocket-list' v-for="item in balances" :key="item.title" @click="buttonClick(item.cmd)">
+        <div>{{ item.title }}</div>
+      </div>
+    </div>
+
+    <div v-else-if="dialogMode === 'balances'">
+      here
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+// import Helper from '../js/helper.js'
+
+export default {
+  props: ['ping'],
+  watch: {
+    ping: function () {
+      this.visible = true
+    }
+  },
+  data() {
+    return {
+      visible: true,
+      dialogMode: 'main',
+      status: '',
+      balances: [{
+        title: '4221.98332',
+        cmd: 'balance'
+      }, {
+        title: '4221.98332',
+        cmd: 'balance'
+      }, {
+        title: '221.98332',
+        cmd: 'balance'
+      }],
+      menuItems: [{
+        title: 'About',
+        cmd: 'about'
+      }, {
+        title: 'Donate',
+        cmd: 'donate'
+      }, {
+        title: 'Send',
+        cmd: 'send'
+      }, {
+        title: 'Message',
+        cmd: 'message'
+      }]
+    }
+  },
+  methods: {
+    buttonClick(id) {
+      switch (id) {
+        case 'close':
+          break
+        case 'send':
+          break
+        case 'balances':
+          break
+        case 'coin-market':
+          break
+        default:
+          console.log('buttonClick not handled: ' + id)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/styles.scss';
+
+.rocket-main {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    padding: 6px;
+    display: flex;
+    -webkit-app-region: drag;
+    padding: 30px;
+
+    .rocket-sidebar {
+        // -webkit-app-region: no-drag;
+        display: flex;
+
+        flex: 0 0 auto;
+        flex-direction: column;
+        background: red;
+    }
+    .rocket-content {
+        flex: 1 1 auto;
+        background: green;
+
+        .rocket-list {
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+        }
+    }
+}
+</style>
